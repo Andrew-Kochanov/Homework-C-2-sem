c82169e9a67ec198045a5ec8dba96840c6180852
Andrew-Kochanov <andkochanov2019@gmail.com>
2026-04-22 00:48:32 +0300

    Formart fix

--- src/CSVPP/main.c
+++ src/CSVPP/main.c
@@ -3,12 +3,12 @@
 
 int main(void)
 {
-    FILE* in = fopen("input.csv","r");
+    FILE* in = fopen("input.csv", "r");
     Table table = { 0 };
     readCSV(in, &table);
     fclose(in);
-    FILE* out = fopen("output.txt","w");
-    printTableToFile(&table,out);
+    FILE* out = fopen("output.txt", "w");
+    printTableToFile(&table, out);
     fclose(out);
     freeTable(&table);
     return 0;
